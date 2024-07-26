eb1d2ea9aa246ef94f8c67b3d1ad0c0d2d58b996
Gabe Dunn <gabe@sent.at>
2024-07-25 23:42:18 -0600

    feat: add flake and build the plugin

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "dracula colorscheme for neovim";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { config, self', inputs', pkgs, system, lib, ... }: {
+        # define the packages provided by this flake
+        packages = {
+          dracula-nvim = pkgs.vimUtils.buildVimPlugin {
+            pname = "dracula-nvim";
+            version = "2024-07-25";
+
+            src = ./.;
+          };
+
+          default = self'.packages.dracula-nvim;
+        };
+      };
+    };
+}
